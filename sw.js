e48a002b8891e4316ffa1e76d37358f5acc67077
Deni Andreawan <andreawandn@gmail.com>
2020-08-27 20:21:40 +0700

    add offline mode

--- sw.js
+++ sw.js
@@ -5,38 +5,43 @@ importScripts(
 if (workbox) {
   console.log("Workbox berhasil dimuat");
 
-  workbox.precaching.precacheAndRoute([
-    { url: "/", revision: "1" },
-    { url: "/manifest.json", revision: "1" },
-    { url: "/index.html", revision: "1" },
-    { url: "/nav.html", revision: "1" },
-    { url: "/pages/home.html", revision: "1" },
-    { url: "/pages/detail.html", revision: "1" },
-    { url: "/pages/favorite.html", revision: "1" },
-    { url: "/js/api", revision: "1" },
-    { url: "/js/db", revision: "1" },
-    { url: "/js/idb", revision: "1" },
-    { url: "/js/jquery.min.js", revision: "1" },
-    { url: "/js/materialize.min.js", revision: "1" },
-    { url: "/js/nav.js", revision: "1" },
-    { url: "/js/sw-register.js", revision: "1" },
-    { url: "/push.js", revision: "1" },
-    { url: "/css/materialize.min.css", revision: "1" },
-    { url: "/css/styles.css", revision: "1" },
-    { url: "/assets/img/apple-touch-icon.png", revision: "1" },
-    { url: "/assets/img/serieA-192", revision: "1" },
-    { url: "/assets/img/serieA-512", revision: "1" },
-    { url: "/favicon.ico", revision: "1" },
+  workbox.precaching.precacheAndRoute(
+    [
+      { url: "/", revision: "1" },
+      { url: "/manifest.json", revision: "1" },
+      { url: "/index.html", revision: "1" },
+      { url: "/nav.html", revision: "1" },
+      { url: "/pages/home.html", revision: "1" },
+      { url: "/pages/detail.html", revision: "1" },
+      { url: "/pages/favorite.html", revision: "1" },
+      { url: "/js/api", revision: "1" },
+      { url: "/js/db", revision: "1" },
+      { url: "/js/idb", revision: "1" },
+      { url: "/js/jquery.min.js", revision: "1" },
+      { url: "/js/materialize.min.js", revision: "1" },
+      { url: "/js/nav.js", revision: "1" },
+      { url: "/js/sw-register.js", revision: "1" },
+      { url: "/push.js", revision: "1" },
+      { url: "/css/materialize.min.css", revision: "1" },
+      { url: "/css/styles.css", revision: "1" },
+      { url: "/assets/img/apple-touch-icon.png", revision: "1" },
+      { url: "/assets/img/serieA-192", revision: "1" },
+      { url: "/assets/img/serieA-512", revision: "1" },
+      { url: "/favicon.ico", revision: "1" },
+      {
+        url: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        revision: "1",
+      },
+      {
+        url:
+          "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+        revision: "1",
+      },
+    ],
     {
-      url: "https://fonts.googleapis.com/icon?family=Material+Icons",
-      revision: "1",
-    },
-    {
-      url:
-        "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
-      revision: "1",
-    },
-  ]);
+      ignoreUrlParametersMatching: [/.*/],
+    }
+  );
 
   workbox.routing.registerRoute(
     new RegExp("https://api.football-data.org/v2"),
